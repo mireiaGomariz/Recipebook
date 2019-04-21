a70c29c930e0462f21f3d1528dcde770037dc7d7
mireiaGomariz <mireiapgomariz@gmail.com>
2019-04-21 17:56:50 +0200

    Modificaciones añadir avatar en los formularios de Registro Edit y New

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_113354) do
+ActiveRecord::Schema.define(version: 2019_04_21_153450) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,10 @@ ActiveRecord::Schema.define(version: 2019_04_09_113354) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.bigint "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
