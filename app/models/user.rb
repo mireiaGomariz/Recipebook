3b2d15d278bebb5c10e12ee27e8fa601aec73151
mireiaGomariz <mireiapgomariz@gmail.com>
2019-04-29 13:16:27 +0200

    Modificación estilos clase avatarImage

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :recipes
 
-  has_attached_file :avatar, styles: { medium: "300x300", thumb: "100x100" }
+  has_attached_file :avatar, styles: { medium: "20x20", thumb: "20x20" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
