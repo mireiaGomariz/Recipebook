7d57a94accdc1b46c2bdd43037d78cb68192ba46
mireiaGomariz <mireiapgomariz@gmail.com>
2019-04-30 21:18:58 +0200

    Cambios en vista show y estilos

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_attached_file :image, styles: { medium: "300x300", thumb: "100x100" }
+  has_attached_file :image, styles: { medium: "22x20", thumb: "50x50" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
